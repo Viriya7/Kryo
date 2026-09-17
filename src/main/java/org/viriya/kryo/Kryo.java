067cc2da938281a8c93ee6df7949940e3d1c6996
Viriya6 <viriyanand4@gmail.com>
2026-09-17 15:04:18 +0700

    First add commit file

---
+++ src/main/java/org/viriya/kryo/Kryo.java
@@ -0,0 +1,17 @@
+package org.viriya.kryo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Kryo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
