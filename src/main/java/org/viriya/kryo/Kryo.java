4b6a68be2872bb3e15022c7b029f04d959f6f669
Viriya6 <viriyanand4@gmail.com>
2026-09-24 20:40:45 +0700

    fahhhh 2

--- src/main/java/org/viriya/kryo/Kryo.java
+++ src/main/java/org/viriya/kryo/Kryo.java
@@ -34,16 +34,17 @@ public final class Kryo extends JavaPlugin {
         WorkbenchManager.init(this);
         SmeltryManager.init(this);
 
-        DustManager.init(this);
         SieveManager.init(this);
+        DustManager.init(this);
         DynamiteManager.init(this);
         IngotManager.init(this);
 
-        getServer().getPluginManager().registerEvents(new SieveManager(), this);
         getServer().getPluginManager().registerEvents(new WorkbenchManager(), this);
+        getServer().getPluginManager().registerEvents(new SmeltryManager(), this);
+        getServer().getPluginManager().registerEvents(new SieveManager(), this);
         getServer().getPluginManager().registerEvents(new DynamiteManager(), this);
         getServer().getPluginManager().registerEvents(new IngotManager(), this);
-        getServer().getPluginManager().registerEvents(new SmeltryManager(), this);
+
 
         PluginCommand kryoCommand = getCommand("kryo");
         if (kryoCommand != null) {
